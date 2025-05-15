9f278c7056acd024b943301c514922eee67a30a0
rish <rishmehta2001@icloud.com>
2025-05-15 16:42:29 -0700

    Revert "Adding mobile vibrate feature"
    This reverts commit 6a24a78ffccbdb809e000ad097e6d8f5186b55ea.

--- src/components/Envelope.js
+++ src/components/Envelope.js
@@ -1,20 +1,9 @@
-// src/components/Envelope.js
 import React from 'react';
 import './Envelope.css';
 
 export default function Envelope({ opened, onToggle }) {
-    // unified handler for touch and click with vibration
-    const handleActivate = () => {
-        if (navigator.vibrate) navigator.vibrate([50, 100, 50]);
-        onToggle();
-    };
-
     return (
-        <div
-            className={`envelope ${opened ? 'opened' : ''}`}
-            onClick={handleActivate}
-            onTouchEnd={handleActivate}
-        >
+        <div className={`envelope ${opened ? 'opened' : ''}`} onClick={onToggle}>
             <div className="flap" />
             <div className="body" />
         </div>
